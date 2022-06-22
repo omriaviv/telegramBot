03e4a0deb6230517d91717a4f42ec89233ca5d94
omriaviv <omriaviv@microsoft.com>
2022-06-22 11:34:21 +0300

    add constants

--- pokerBot/main.py
+++ pokerBot/main.py
@@ -3,7 +3,6 @@ import os
 from datetime import datetime
 
 import telebot
-from telebot import TeleBot
 
 from pokerBot.db.dbService import *
 from pokerBot.model import *
@@ -14,25 +13,24 @@ teleBot = telebot.TeleBot(API_KEY)
 
 class Main(object):
     def __init__(self, bot):
-        # type: (TeleBot) -> None
         self._bot = bot
         self.db_service = DbService()
 
     def start(self, message):
         if self.db_service.get_started_timestamp() > 0:
-            self._bot.send_message(message.chat.id, "\U0001F92A Game already started, please run /end to end game")
+            self._bot.send_message(message.chat.id, f"{ZANY_FACE} Game already started, please run "
+                                                    f"/{END_COMMAND} to end game")
             return
 
         players = self.db_service.get_all_players()
         if len(players) == 0:
-            self._bot.send_message(message.chat.id, "No players were added, please run /add-player [name]")
+            self._bot.send_message(message.chat.id, f"No players were added, please run /{ADD_PLAYER_COMMAND} [name]")
             return
 
         self.init_players()
         self.db_service.insert_started_timestamp(datetime.now().replace(microsecond=0).timestamp())
-        self._bot.send_message(message.chat.id, "Welcome to the Poker Game!"
-                                                "\n\U00002665 \U00002660 \U00002666 \U00002663")
-        self._bot.send_message(message.chat.id, "\U00002705 Started!")
+        self._bot.send_message(message.chat.id, f"Welcome to the Poker Game!\n{CARDS_EMOJI}")
+        self._bot.send_message(message.chat.id, f"{CHECK_MARK_GREEN} Started!")
         self.send_jackpot(message)
 
     def infinity_polling(self):
@@ -43,18 +41,18 @@ class Main(object):
         if started_timestamp == 0:
             return
 
-        self._bot.send_message(message.chat.id, "\U0001f480 Game over!")
+        self._bot.send_message(message.chat.id, f"{SKULL} Game over!")
 
         start_time = datetime.fromtimestamp(started_timestamp)
         end_time = datetime.now().replace(microsecond=0)
         total_time = end_time - start_time
-        self._bot.send_message(message.chat.id, f"\U000023F1 Total Game Time: {total_time}")
+        self._bot.send_message(message.chat.id, f"{STOPWATCH} Total Game Time: {total_time}")
         self.status(message)
         self.db_service.remove_started_timestamp(started_timestamp)
         self._bot.send_message(message.chat.id, "Run winners command: /winners [name:chips] [name:chips] ...")
 
     def add_player(self, message):
-        name = self.parse_player_name(message, "/add-player")
+        name = self.parse_player_name(message, ADD_PLAYER_COMMAND)
         if not name:
             return
 
@@ -63,16 +61,16 @@ class Main(object):
             player = Player(name)
             self.db_service.insert_player(player)
             self._bot.send_message(message.chat.id,
-                                   f"Player added successfully:\n{player}")
+                                   f"{CHECK_MARK_BLACK} {player} was added successfully")
         else:
             self._bot.send_message(message.chat.id,
-                                   f"\U0001F6AB Player {name} already exist, please choose another name")
+                                   f"{PROHIBITED} Player {name} already exist, please choose another name")
 
     def is_game_started(self, chat_id) -> float:
         started_timestamp = self.db_service.get_started_timestamp()
         if started_timestamp == 0:
-            self._bot.send_message(chat_id, "\U0001F6AB Game has not started!")
-            self._bot.send_message(chat_id, "Please run: /start")
+            self._bot.send_message(chat_id, f"{PROHIBITED} Game has not started!")
+            self._bot.send_message(chat_id, f"Please run: /{START_COMMAND}")
 
         return started_timestamp
 
@@ -91,7 +89,8 @@ class Main(object):
     def parse_rebuy(self, message) -> Player:
         split = message.text[1:].split(' ')
         if len(split) != 2:
-            self._bot.send_message(message.chat.id, "\U0001F6AB Invalid command, please try again: /rebuy [name]")
+            self._bot.send_message(message.chat.id, f"{PROHIBITED} Invalid command, please try again: "
+                                                    f"/{RE_BUY_COMMAND} [name]")
             return None
 
         player_name = split[1]
@@ -167,13 +166,13 @@ class Main(object):
 
             # todo calculate owes to by game/food
 
-            self._bot.send_message(message.chat.id, f"\U0001F3C6 {winner} won {player.win_payment.amount} \U0001F4B5")
+            self._bot.send_message(message.chat.id, f"{TROPHY} {winner} won {player.win_payment.amount} {MONEY}")
 
         jackpot = self.get_jackpot()
         total_wins = self.get_total_wins()
         if total_wins > jackpot:
             self._bot.send_message(message.chat.id,
-                                   f"\U0001F6AB Invalid wins amount, "
+                                   f"{PROHIBITED} Invalid wins amount, "
                                    f"total wins: {total_wins}, jackpot: {jackpot}")
         else:
             if total_wins < jackpot:
@@ -188,7 +187,7 @@ class Main(object):
 
     @staticmethod
     def format_jackpot(jackpot: float):
-        return f"{JACKPOT}: {jackpot} \U0001F4B5\nTotal Chips: {int(jackpot / GAME * CHIPS)}"
+        return f"{JACKPOT}: {jackpot} {MONEY}\nTotal Chips: {int(jackpot / GAME * CHIPS)}"
 
     def get_jackpot(self) -> float:
         jackpot = 0
@@ -207,13 +206,14 @@ class Main(object):
     def parse_player_name(self, message, command) -> str:
         split = message.text.split(' ')
         if len(split) < 2:
-            self._bot.send_message(message.chat.id, f"\U0001F6AB Invalid command, please try again: {command} [name]")
+            self._bot.send_message(message.chat.id, f"{PROHIBITED} "
+                                                    f"Invalid command, please try again: /{command} [name]")
             return ''
 
         return split[1]
 
     def remove_player(self, message):
-        name = self.parse_player_name(message, "/remove-player")
+        name = self.parse_player_name(message, REMOVE_PLAYER_COMMAND)
         if not name:
             return
 
@@ -223,30 +223,31 @@ class Main(object):
             return
 
         if self.db_service.remove_player(name):
-            self._bot.send_message(message.chat.id, f"\U00002714 {name} was removed")
+            self._bot.send_message(message.chat.id, f"{CHECK_MARK_BLACK} {name} was removed")
 
     def help(self, message):
-        help_str = f"- /start - start a game" \
-                   f"\n- /status - get players status" \
-                   f"\n- /[player_name1,player_name2,...] - enter players names" \
-                   f"\n- /rebuy [player_name] - add re-buy to player" \
-                   f"\n- /food [player_name] - add food order by player" \
-                   f"\n- /end - end game" \
-                   f"\n- /winners [player_name1:chips player_name2:chips ...] - enter the winners"
+        help_str = f"- /{START_COMMAND} - start a game" \
+                   f"\n- /{STATUS_COMMAND} - get players status" \
+                   f"\n- /{RE_BUY_COMMAND} [player_name] - add re-buy to player" \
+                   f"\n- /{FOOD_COMMAND} [player_name] - add food order by player" \
+                   f"\n- /{END_COMMAND} - end game" \
+                   f"\n- /{WINNERS_COMMAND} [player_name1:chips player_name2:chips ...] - enter the winners" \
+                   f"\n- /{ADD_PLAYER_COMMAND} [player_name]" \
+                   f"\n- /{REMOVE_PLAYER_COMMAND} [player_name]"
 
-        self._bot.send_message(message.chat.id, f"\U0001F4D6\n{help_str}")
+        self._bot.send_message(message.chat.id, f"{BOOK}\n{help_str}")
 
     def send_invalid_food_command(self, chat_id):
         self._bot.send_message(chat_id,
-                               "\U0001F6AB Invalid command, please try again: /food [name] [amount]")
+                               f"{PROHIBITED} Invalid command, please try again: /{FOOD_COMMAND} [name] [amount]")
 
     def send_invalid_winners_command(self, chat_id):
         self._bot.send_message(chat_id,
-                               "\U0001F6AB Invalid command, "
+                               f"{PROHIBITED} Invalid command, "
                                "please try again: /winners [name:chips] [name:chips] ...")
 
     def send_player_does_not_exist(self, chat_id, player_name):
-        self._bot.send_message(chat_id, f"\U0001F6AB {player_name} doesn't exist")
+        self._bot.send_message(chat_id, f"{PROHIBITED} {player_name} doesn't exist")
 
     def init_players(self):
         players = self.db_service.get_all_players()
@@ -254,44 +255,49 @@ class Main(object):
         [self.db_service.update_player(p) for p in players]
 
 
-@teleBot.message_handler(commands=['start'])
+@teleBot.message_handler(commands=[START_COMMAND])
 def start(message):
     main.start(message)
 
 
-@teleBot.message_handler(commands=['end'])
+@teleBot.message_handler(commands=[END_COMMAND])
 def end(message):
     main.end(message)
 
 
-@teleBot.message_handler(commands=['rebuy'])
+@teleBot.message_handler(commands=[RE_BUY_COMMAND])
 def re_buy(message):
     main.re_buy(message)
 
 
-@teleBot.message_handler(commands=['status'])
+@teleBot.message_handler(commands=[STATUS_COMMAND])
 def status(message):
     main.status(message)
 
 
-@teleBot.message_handler(commands=['food'])
+@teleBot.message_handler(commands=[FOOD_COMMAND])
 def food(message):
     main.food_order(message)
 
 
-@teleBot.message_handler(commands=['winners'])
+@teleBot.message_handler(commands=[WINNERS_COMMAND])
 def winners(message):
     main.winners(message)
 
 
-@teleBot.message_handler(commands=['add-player'])
+@teleBot.message_handler(commands=[ADD_PLAYER_COMMAND])
 def add_player(message):
     main.add_player(message)
 
 
-@teleBot.message_handler(commands=['remove-player'])
+@teleBot.message_handler(commands=[REMOVE_PLAYER_COMMAND])
 def remove_player(message):
     main.remove_player(message)
+
+
+@teleBot.message_handler(commands=[HELP_COMMAND])
+def help_command(message):
+    main.help(message)
 
 
 if __name__ == '__main__':
