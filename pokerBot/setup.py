0cc8e44076f7fc228c46348d00e9d49f7954de76
omriaviv <omriaviv@microsoft.com>
2022-06-22 16:35:15 +0300

    add setup.py

---
+++ pokerBot/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pyTelegramPokerBot',
+    version='0.1.0',
+    author='Omri Aviv',
+    author_email='omriaviv@microsoft.com',
+    # packages=['poker_bot'],
+    url='https://github.com/omriaviv/telegramBot',
+    description='Telegram Bot for Poker with friends',
+    install_requires=[
+        "pyTelegramBotAPI"
+    ],
+)
